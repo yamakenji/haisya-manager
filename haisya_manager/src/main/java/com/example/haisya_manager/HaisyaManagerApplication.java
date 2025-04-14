1142cecf789c6eb89dfdf220dbf97d9062d24dac
kodai9263 <kou92638685@gmail.com>
2025-04-14 15:32:55 +0900

    first commit

---
+++ haisya_manager/src/main/java/com/example/haisya_manager/HaisyaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.haisya_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaisyaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaisyaManagerApplication.class, args);
+	}
+
+}
---
+++ haisya_manager/src/main/java/com/example/haisya_manager/entity/Member.java
@@ -0,0 +1,5 @@
+package com.example.haisya_manager.entity;
+
+public class Member {
+
+}
