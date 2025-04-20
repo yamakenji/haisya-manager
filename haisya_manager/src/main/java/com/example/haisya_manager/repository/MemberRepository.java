14765ce27749a97c5d55d123fa30b6da1a15b6dd
kodai9263 <kou92638685@gmail.com>
2025-04-20 13:11:01 +0900

    リポジトリ ログイン画面表示まで作成

---
+++ haisya_manager/src/main/java/com/example/haisya_manager/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.haisya_manager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.haisya_manager.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+
+}
